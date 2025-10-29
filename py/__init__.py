bb3e1312cfffe20e2b31657edaf6560f636850ef
WaltomAdaam2 <waltomadaam@gmail.com>
2025-10-29 21:30:50 +0800

    Update __init__.py

--- py/__init__.py
+++ py/__init__.py
@@ -4,12 +4,13 @@ A PyQt5-based desktop network traffic monitoring floating window
 """
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
+__author__ = "WaltomAdaam2"
 __description__ = "Network traffic monitoring floating window"
 
 from .config_manager import ConfigManager
 from .network_monitor import NetworkMonitor
 from .auto_start_manager import AutoStartManager
 from .ui_painter import UIPainter
+
 
 __all__ = ['ConfigManager', 'NetworkMonitor', 'AutoStartManager', 'UIPainter']
